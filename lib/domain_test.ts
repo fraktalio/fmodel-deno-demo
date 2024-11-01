82aba1bd25391db216af16bbe963964eaf02e0a1
Ivan Dugalic <idugalic@gmail.com>
2024-11-01 14:18:39 +0100

    more tests...

--- lib/domain_test.ts
+++ lib/domain_test.ts
@@ -1,18 +1,31 @@
-import { restaurantDecider, restaurantView } from "./domain.ts";
 import {
+  orderDecider,
+  orderView,
+  restaurantDecider,
+  restaurantView,
+} from "./domain.ts";
+import {
+  orderCommandSchema,
+  orderEventSchema,
   restaurantCommandSchema,
   restaurantEventSchema,
 } from "./api_schema.ts";
 import { ViewSpecification } from "../test_specification.ts";
 import { DeciderSpecification } from "../test_specification.ts";
-import type { RestaurantCommand, RestaurantEvent } from "./api.ts";
+import type {
+  OrderCommand,
+  OrderEvent,
+  RestaurantCommand,
+  RestaurantEvent,
+} from "./api.ts";
 
+// #########################################################################
 // A convinient testing specififcation for the Decider - GIVEN / WHEN / THEN
+// #########################################################################
 
 // Json representation of the command
 const createRestaurantCommandJson = `
  {
-     "commandId": "691490bb-c4d3-45b8-99d0-efcf20e353ag",
      "decider": "Restaurant",
      "kind": "CreateRestaurantCommand",
      "id": "691490bb-c4d3-45b8-99d0-efcf20e353ao",
@@ -52,7 +65,6 @@ const restaurantCreatedEventJson = `
 // Json representation of the command
 const changeRestaurantMenuCommandJson = `
     {
-        "commandId": "691490bb-c4d3-45b8-99d0-efcf20e353ag",
         "decider": "Restaurant",
         "kind": "ChangeRestaurantMenuCommand",
         "id": "691490bb-c4d3-45b8-99d0-efcf20e353ao",
@@ -70,7 +82,6 @@ const changeRestaurantMenuCommandJson = `
 
 const placeOrderCommandJson = `
     {
-        "commandId": "691490bb-c4d3-45b8-99d0-efcf20e353ag",
         "decider": "Restaurant",
         "kind": "PlaceOrderCommand",
         "id": "691490bb-c4d3-45b8-99d0-efcf20e353ao",
@@ -80,6 +91,28 @@ const placeOrderCommandJson = `
           {"menuItemId": "2", "name": "Soup2", "price": "16.94"},
           {"menuItemId": "3", "name": "Steak2", "price": "19.89"}
         ]
+    }
+    `;
+
+const createOrderCommandJson = `
+    {
+        "decider": "Order",
+        "kind": "CreateOrderCommand",
+        "restaurantId": "691490bb-c4d3-45b8-99d0-efcf20e353ao",
+        "id": "691490bb-c4d3-45b8-99d0-efcf20e353ag",
+        "menuItems": [
+          {"menuItemId": "1", "name": "Salad2", "price": "18.59"},
+          {"menuItemId": "2", "name": "Soup2", "price": "16.94"},
+          {"menuItemId": "3", "name": "Steak2", "price": "19.89"}
+        ]
+    }
+    `;
+
+const markOrderAsPreparedCommandJson = `
+    {
+        "decider": "Order",
+        "kind": "MarkOrderAsPreparedCommand",
+        "id": "691490bb-c4d3-45b8-99d0-efcf20e353ag"
     }
     `;
 
@@ -110,7 +143,7 @@ const restaurantOrderPlacedEventJson = `
       "kind": "RestaurantOrderPlacedEvent",
       "id": "691490bb-c4d3-45b8-99d0-efcf20e353ao",
       "orderId": "691490bb-c4d3-45b8-99d0-efcf20e353ag",
-       "menuItems": [
+      "menuItems": [
           {"menuItemId": "1", "name": "Salad2", "price": "18.59"},
           {"menuItemId": "2", "name": "Soup2", "price": "16.94"},
           {"menuItemId": "3", "name": "Steak2", "price": "19.89"}
@@ -132,6 +165,32 @@ const restaurantOrderNotPlacedEventJson = `
         ],
       "final": false,
       "reason": "Restaurant does not exist!"
+  }
+  `;
+
+const orderCreatedEventJson = `
+  {
+      "version": 1,
+      "decider": "Order",
+      "kind": "OrderCreatedEvent",
+      "restaurantId": "691490bb-c4d3-45b8-99d0-efcf20e353ao",
+      "id": "691490bb-c4d3-45b8-99d0-efcf20e353ag",
+      "menuItems": [
+          {"menuItemId": "1", "name": "Salad2", "price": "18.59"},
+          {"menuItemId": "2", "name": "Soup2", "price": "16.94"},
+          {"menuItemId": "3", "name": "Steak2", "price": "19.89"}
+        ],
+      "final": false
+  }
+  `;
+
+const orderPreparedEventJson = `
+  {
+      "version": 1,
+      "decider": "Order",
+      "kind": "OrderPreparedEvent",
+      "id": "691490bb-c4d3-45b8-99d0-efcf20e353ag",
+      "final": false
   }
   `;
 
@@ -176,7 +235,7 @@ Deno.test(function changeRestaurantMenuDeciderTest() {
       JSON.parse(restaurantMenuChangedEventJson),
     );
 
-  // Run the second test specification for the restaurant decider
+  // Run the test specification for the restaurant decider
   DeciderSpecification.for(restaurantDecider)
     .given([restaurantCreatedEvent])
     .when(changeRestaurantMenuCommand)
@@ -203,7 +262,7 @@ Deno.test(function placeOrderAtRestaurantDeciderTest() {
       JSON.parse(restaurantOrderPlacedEventJson),
     );
 
-  // Run the second test specification for the restaurant decider
+  // Run the test specification for the restaurant decider
   DeciderSpecification.for(restaurantDecider)
     .given([restaurantCreatedEvent])
     .when(placeOrderCommand)
@@ -224,13 +283,64 @@ Deno.test(function placeOrderAtRestaurantWithErrorDeciderTest() {
       JSON.parse(restaurantOrderNotPlacedEventJson),
     );
 
-  // Run the second test specification for the restaurant decider
+  // Run the test specification for the restaurant decider
   DeciderSpecification.for(restaurantDecider)
     // Given NO restaurant previously created!!!
     .given([])
     .when(placeOrderCommand)
     .then([restaurantOrderNotPlacedEvent]);
 });
+
+// The Decider test for the create order command
+Deno.test(function createOrderDeciderTest() {
+  // Parse the order command from the request / Zod validation/parsing
+  const createOrderCommand: OrderCommand = orderCommandSchema
+    .parse(
+      JSON.parse(createOrderCommandJson),
+    );
+
+  // Parse the order event.
+  const orderCreatedEvent: OrderEvent = orderEventSchema
+    .parse(
+      JSON.parse(orderCreatedEventJson),
+    );
+
+  // Run the test specification for the order decider
+  DeciderSpecification.for(orderDecider)
+    .given([])
+    .when(createOrderCommand)
+    .then([orderCreatedEvent]);
+});
+
+// The Decider test for the create order command
+Deno.test(function markOrderAsPeparedDeciderTest() {
+  // Parse the order command from the request / Zod validation/parsing
+  const markOrderAsPreparedCommand: OrderCommand = orderCommandSchema
+    .parse(
+      JSON.parse(markOrderAsPreparedCommandJson),
+    );
+
+  // Parse the order event.
+  const orderCreatedEvent: OrderEvent = orderEventSchema
+    .parse(
+      JSON.parse(orderCreatedEventJson),
+    );
+  // Parse the order event.
+  const orderPreparedEvent: OrderEvent = orderEventSchema
+    .parse(
+      JSON.parse(orderPreparedEventJson),
+    );
+
+  // Run the test specification for the order decider
+  DeciderSpecification.for(orderDecider)
+    .given([orderCreatedEvent])
+    .when(markOrderAsPreparedCommand)
+    .then([orderPreparedEvent]);
+});
+
+// ###############################################################
+// A convinient testing specififcation for the View - GIVEN / THEN
+// ###############################################################
 
 Deno.test(function reastaurantCreatedViewTest() {
   // Parse the restaurant event.
@@ -283,6 +393,31 @@ Deno.test(function restaurantMenuChangedViewTest() {
         menuId: "34110e19-ca72-45e7-b969-61bebf54da08",
         cuisine: "SERBIAN",
       },
+    });
+});
+
+Deno.test(function orderPreparedViewTest() {
+  // Parse the order event.
+  const orderCreatedEvent: OrderEvent = orderEventSchema
+    .parse(
+      JSON.parse(orderCreatedEventJson),
+    );
+  const orderPreparedEvent: OrderEvent = orderEventSchema
+    .parse(
+      JSON.parse(orderPreparedEventJson),
+    );
+
+  ViewSpecification.for(orderView)
+    .given([orderCreatedEvent, orderPreparedEvent])
+    .then({
+      orderId: "691490bb-c4d3-45b8-99d0-efcf20e353ag",
+      restaurantId: "691490bb-c4d3-45b8-99d0-efcf20e353ao",
+      menuItems: [
+        { menuItemId: "1", name: "Salad2", price: "18.59" },
+        { menuItemId: "2", name: "Soup2", price: "16.94" },
+        { menuItemId: "3", name: "Steak2", price: "19.89" },
+      ],
+      status: "PREPARED",
     });
 });
 
